788b27a4af99f1e9d70a62748a17ddbbe87a90f1
Joni Pöllänen <joni.pollanen2@gmail.com>
2020-01-21 12:17:40 +0200

    Updates

---
+++ precache-manifest.aa677d1fc4634b86bda6e6f75772a033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6081d835d66f7024df30d6d3ce3e0c",
+    "url": "/teatteri/index.html"
+  },
+  {
+    "revision": "a90c89084d1fb121b1bf",
+    "url": "/teatteri/static/js/2.b8dfffd3.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/teatteri/static/js/2.b8dfffd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "131075f5937cc5cd96ff",
+    "url": "/teatteri/static/js/main.8f03a7df.chunk.js"
+  },
+  {
+    "revision": "af3706214396c5f9549b",
+    "url": "/teatteri/static/js/runtime-main.2a53405a.js"
+  }
+]);
