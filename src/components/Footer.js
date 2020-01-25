b455ed7f931bbd39588914a1d04fc50816d113b5
Joni Pöllänen <joni.pollanen2@gmail.com>
2020-01-25 12:34:44 +0200

    style trying out new fontSizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,18 @@ const Footer = () => {
 
   return(
     <div style={{marginTop: '3vh', marginBottom: '3vh', textAlign: 'center'}}>
-      <p style={{fontSize: '3.5vw', fontWeight: 'bold'}}>
-        Tiedustelut ja ryhmävaraukset puh. 040 703 7478 <br />
+      <p style={{fontSize: '2em', fontWeight: 'bold'}}>
+        Tiedustelut ja ryhmävaraukset <br /> 
+        puh. 040 703 7478 <br />
         Ristinkyläntie 1, Liperi
       </p>
       <div style={{marginTop: '1vh', textAlign: 'center'}}>
         <a 
           href='https://www.facebook.com/ristin.kesateatteri' 
           target='_blank' 
-          rel='noopener'>
-            <img src={fbLogo} style={{width: '4vw', height: '4vw'}}/>Teatterin facebook sivuille
+          rel='noopener'
+          style={{fontSize: '1.3em'}}>
+            <img src={fbLogo} style={{width: '2em', height: '2em'}}/>Teatterin facebook sivuille
         </a>
       </div>
     </div>
