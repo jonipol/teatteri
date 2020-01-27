c0c10ee739797f8460fb7e34877179b76ec6363e
Joni Pöllänen <joni.pollanen2@gmail.com>
2020-01-27 12:37:30 +0200

    feat: Past shows added

--- src/components/Past.js
+++ src/components/Past.js
@@ -2,11 +2,80 @@ import React from 'react';
 
 const Past = () => {
 
+  const listStyle = {
+    listStyleType: 'none',
+  };
+
+  const column = {
+    textAlign: 'left',
+    display: 'table',
+  };
+
+  const leftColumn = {
+    float: 'left',
+  };
 
   return(
-    <div>
+    <div style={{margin: 'auto'}}>
       <h1>Aiempaa ohjelmistoa</h1>
-      
+      <div>
+      <div style={column, leftColumn}>
+        <ul style={listStyle}>
+          <li>2019 Ei meidän Maija!</li>
+          <li>2018 Remontti</li>
+          <li>2017 Rovaniemen markkinoilla</li>
+          <li>2016 Juonittelua ja onnen kauppaa</li>
+          <li>2015 Tahdotko naimisiin?</li>
+          <li>2014 Siltalan pehtoori</li>
+          <li>2013 Kulkurin valssi eli Koivusaaren kartanon salaisuus</li>
+          <li>2012 Kätkäläinen ja musta koira (kantaesitys)</li>
+          <li>2011 Akaton mies</li>
+          <li>2010 Hotelli Humina</li>
+          <li>2009 Kätkäläinen ja naapurin mies</li>
+          <li>2008 Kätkäläinen ja kaupungin valot</li>
+          <li>2007 Tulitikut lainassa Piiroisen pässillä</li>
+          <li>2006 Tauno Kapalo ja ikuinen ansa</li>
+          <li>2005 Konsultti</li>
+          <li>2004 Hurmuri tuli taloon</li>
+          <li>2003 Mies joka ei osannut sanoa ei</li>
+          <li>2002 Jätkät</li>
+          <li>2001 Kätkäläinen ja ulkomaan elävä (kantaesitys)</li>
+          <li>2000 Rajaralli</li>
+          <li>1999 Into piukassa</li>
+          <li>1998 Isä Camillon kylä</li>
+          <li>1997 Kätkäläisen jättipotti</li>
+          <li>1996 Rakas lotta</li>
+        </ul>
+      </div>
+      <div style={column}>
+        <ul style={listStyle}>
+          <li>1995 Kätkäläinen</li>
+          <li>1994 Kun ruusut kukkivat</li>
+          <li>1993 Vetovoima eli vetovaimo</li>
+          <li>1992 Yllätyslapsi</li>
+          <li>1991 Tuntematon potilas</li>
+          <li>1990 Tulitikkuja lainaamassa</li>
+          <li>1989 Kettuvaari</li>
+          <li>1988 Täystusina toivosia</li>
+          <li>1987 Suomessa olen minä syntynyt</li>
+          <li>1986 Viisas neitsyt</li>
+          <li>1985 Punapirtin humaniitta</li>
+          <li>1984 Pitkäjärveläiset</li>
+          <li>1983 Kylätanssit</li>
+          <li>1982 Kilpakosijat</li>
+          <li>1981 Kiljusen herrasväki</li>
+          <li>1980 Kyläkauppa</li>
+          <li>1979 Susipari</li>
+          <li>1979 Nalle Puh</li>
+          <li>1978 Kylän viimeinen nainen</li>
+          <li>1977 Pirttipohjalaiset</li>
+          <li>1976 Ruoska ja rakkaus</li>
+          <li>1975 Roinilan talossa</li>
+          <li>1974 Ikiliikkuja</li>
+          <li><nobr>1973 Rakkaus on nopeampi Piiroisen pässiä</nobr></li>
+        </ul>
+      </div>
+      </div>
     </div>
   );
 };
